1cdbea51d9951f0387738910b9c623daccfdb911
rledford <ryan.c.ledford@gmail.com>
2020-01-23 13:23:11 -0600

    changed host to readerIp and req timeout

--- src/HttpReaderHelpers.ts
+++ src/HttpReaderHelpers.ts
@@ -4,12 +4,13 @@ import fs from 'fs';
 const READER_API_URL = '/cgi-bin/index.cgi';
 
 function restartReader(
-  host: string,
+  readerIp: string,
   username: string = 'root',
   password: string = 'impinj'
 ): rp.RequestPromise {
-  const url = `http://${host}${READER_API_URL}`;
+  const url = `http://${readerIp}${READER_API_URL}`;
   return rp.post(url, {
+    timeout: 5000,
     form: {
       'reboot.x': '22',
       'reboot.y': '3'
@@ -22,14 +23,15 @@ function restartReader(
 }
 
 function uploadUpg(
-  host: string,
+  readerIp: string,
   path: string,
   username: string = 'root',
   password: string = 'impinj'
 ): rp.RequestPromise {
-  const url = `http://${host}${READER_API_URL}`;
+  const url = `http://${readerIp}${READER_API_URL}`;
 
   return rp.post(url, {
+    timeout: 5000,
     formData: {
       file: fs.createReadStream(path)
     },
@@ -41,16 +43,17 @@ function uploadUpg(
 }
 
 function provisionReader(
-  host: string,
+  readerIp: string,
   readerName: string,
   readerAgentToken: string,
   itemsenseHost: string,
   itemsenseCert: string
 ): rp.RequestPromise {
-  const url = `https://${host}:51505/provision`;
+  const url = `https://${readerIp}:51505/provision`;
   const itemsenseUrl = `https://${itemsenseHost}/itemsense`;
 
   return rp.post(url, {
+    timeout: 5000,
     followRedirect: false,
     strictSSL: false,
     headers: {
